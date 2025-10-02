9c88bab27942d17bdb89e973d3d47d6f077c6e51
Fkarau <30944677+Karau1218@users.noreply.github.com>
2025-10-01 18:30:10 -0700

    Done with List practice, ArrayPractice, Set practice

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -1,18 +1,25 @@
-public class ArrayPractice {
+public class ArrayPractice { //DONE WITH FILE DELETE THIS PART
   public static void main(String[] args) {
     // Create an array of Strings of size 4
-
+    int [] numbers = new int[4];
     // Set the value of the array at each index to be a different String
     // It's OK to do this one-by-one
-
+    numbers[0] = 10;
+    numbers[1] = 20;    
+    numbers[2] = 30;
+    numbers[3] = 40;
     // Get the value of the array at index 2
-
+    System.out.println("The value at index 2 is " + numbers[2]);
     // Get the length of the array
-
+    System.out.println("The length of the array is " + numbers.length);
     // Iterate over the array using a traditional for loop and print out each item
-
+    for (int i = 0; i < numbers.length; i++) {
+      System.out.println("The value at index " + i + " is " + numbers[i]);
+    }
     // Iterate over the array using a for-each loop and print out each item
-
+    for (int i = 0; i < numbers.length; i++) {
+      System.out.println("The value at index " + i + " is " + numbers[i]);
+    }
     /*
      * Reminder!
      * 
