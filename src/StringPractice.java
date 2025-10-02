a181911030bc8cd765d1a7410c61c9c47659b8e5
Fkarau <30944677+Karau1218@users.noreply.github.com>
2025-10-01 18:32:42 -0700

    Done with String practice

--- src/StringPractice.java
+++ src/StringPractice.java
@@ -7,11 +7,20 @@ public class StringPractice {
       System.out.println("The length of " + fruit + " is " + fruit.length());
     }
     // Concatenate (add) two strings together and reassign the result
-
+    String joinString = String.join(", ", fruits);
+    System.out.println(joinString);
     // Find the value of the character at index 3
-
+    String fruit = fruits[0];
+    System.out.println("The character at indeAx 3 of " + fruit + " is " + fruit.charAt(3));
     // Check whether the string contains a given substring (i.e. does the string have "abc" in it?)
-
+    for (String fruit : fruits) {
+      if (fruit.contains("abc")) {
+        System.out.println("The string contains abc");
+      } else {
+        System.out.println("The string does not contain abc");
+      }
+    }
+  
     // Iterate over the characters of the string, printing each one on a separate line
 
     // Create an ArrayList of Strings and assign it to a variable
